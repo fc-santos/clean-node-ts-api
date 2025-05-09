af7d8db388ce7d1ab3e5ab9407eef97d4d4a9f54
fc-santos <filipesantos@live.ca>
2025-05-09 11:12:34 -0400

    chore: add jest configs and scripts

---
+++ jest-integration-config.js
@@ -0,0 +1,8 @@
+// const config = require("./jest.config");
+// config.testMatch = ["**/*.spec.ts"];
+// module.exports = config;
+import config from "./jest.config.js";
+
+config.testMatch = ["**/*.test.ts"];
+
+export default config;
---
+++ jest-unit-config.js
@@ -0,0 +1,8 @@
+// const config = require("./jest.config");
+// config.testMatch = ["**/*.spec.ts"];
+// module.exports = config;
+import config from "./jest.config.js";
+
+config.testMatch = ["**/*.spec.ts"];
+
+export default config;
