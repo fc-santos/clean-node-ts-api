d602840a68ee917dca602cfdd09801435da46e2f
fc-santos <filipesantos@live.ca>
2025-05-09 13:45:38 -0400

    refactor: group all protocols in a single file

---
+++ src/data/usecases/add-account/db-add-account-protocols.spec.ts
@@ -0,0 +1,7 @@
+import * as DbAddAccountProtocols from "./db-add-account-protocols"
+
+describe("DbAddAccountProtocols", () => {
+  test("Should have exports", () => {
+    expect(DbAddAccountProtocols).toEqual(expect.any(Object))
+  })
+})
